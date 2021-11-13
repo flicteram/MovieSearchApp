f561b558414c50471a1b6b46e5ac5e0671369724
Flicter Alexandru <flicteralexandru@gmail.com>
2021-11-13 11:36:16 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import './index.css'
 
 
 ReactDOM.render(
@@ -10,3 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//4fd39374b175ef0640037cc65b89f715
